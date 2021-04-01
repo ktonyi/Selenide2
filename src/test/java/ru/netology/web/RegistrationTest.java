01d45c7bd2aaaed76151c195a86f6551bf9c64a3
ktonyi <antonigorevich.k@gmail.com>
2021-04-02 01:30:17 +0300

    registrTEst

--- src/test/java/ru/netology/web/RegistrationTest.java
+++ src/test/java/ru/netology/web/RegistrationTest.java
@@ -11,8 +11,8 @@ import static com.codeborne.selenide.Selenide.*;
 class RegistrationTest {
     @Test
     void shouldRegisterByAccountNumberDOMModification() {
-        open("http://localhost:9999");
-        $$(".tab-item").find(exactText("По номеру счёта")).click();
+        open("http://localhost:9999");// открываем страницу
+        $$(".tab-item").find(exactText("По номеру счёта")).click();// находим элемент и кликаем по нему
         $("[name='number']").setValue("4055 0100 0123 4613 8564");
         $("[name='phone']").setValue("+792000000000");
         $$("button").find(exactText("Продолжить")).click();
